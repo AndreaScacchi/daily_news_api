295c9d0dfb63ed02b4af8ae3eaa8ba16b9b6fef8
Andrea <andreascacchi10@gmail.com>
2023-09-16 17:21:23 +0200

    body message modified

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ content = request.json()
 
 # access the article titles and description
 body = ""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + "\n" + article["url"] + 2*"\n"
+        body = "Subject: Today's news " + "\n" + body + article["title"] + "\n" + article["description"] + "\n" + article["url"] + 2*"\n"
         
 body = body.encode("utf-8")
 send_email(message=body)
