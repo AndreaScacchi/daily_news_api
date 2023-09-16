e248d44e2f6837d6e08535d382c55ee77d0d008c
Andrea <andreascacchi10@gmail.com>
2023-09-16 17:01:44 +0200

    email receiver added

--- send_email.py
+++ send_email.py
@@ -1,15 +1,16 @@
 import smtplib, ssl
+import config
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     
-    username = "app8flask@gmail.com"
-    password = "your_gmail_app_password"
+    username = "andreascacchi10@gmail.com"
+    email_password = config.email_password
     
-    receiver = "app8flask@gmail.com"
+    receiver = "andreascacchi10@gmail.com"
     context = ssl.create_default_context()
     
     with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, password)
+        server.login(username, email_password)
         server.sendmail(username, receiver, message)
