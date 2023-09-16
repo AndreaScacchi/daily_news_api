729a53f34c3eeaf6b2efbd1a0f49c7984aa258fa
Andrea <andreascacchi10@gmail.com>
2023-09-16 17:10:08 +0200

    app password changed

--- send_email.py
+++ send_email.py
@@ -5,12 +5,12 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     
-    username = "andreascacchi10@gmail.com"
-    email_password = config.email_password
+    username = config.username
+    app_password = config.email_password
     
-    receiver = "andreascacchi10@gmail.com"
+    receiver = config.username
     context = ssl.create_default_context()
     
     with smtplib.SMTP_SSL(host, port, context=context) as server:
-        server.login(username, email_password)
+        server.login(username, app_password)
         server.sendmail(username, receiver, message)
